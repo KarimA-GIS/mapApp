880889ee8f8d98e39ef18d9f7eabe4f7b8d5c2ea
KarimA-GIS <kerim120199@gmail.com>
2020-04-28 16:46:20 -0700

    Updates

---
+++ precache-manifest.9cad4e989c8346fd60a49b421278688c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f5134b98f34a47bf822e9bb5d4848f",
+    "url": "/KarimA-GIS/mapApp/index.html"
+  },
+  {
+    "revision": "376fdad977c71dee9ede",
+    "url": "/KarimA-GIS/mapApp/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "bcd10310c6b42591ee2f",
+    "url": "/KarimA-GIS/mapApp/static/js/2.9521fbcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KarimA-GIS/mapApp/static/js/2.9521fbcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "376fdad977c71dee9ede",
+    "url": "/KarimA-GIS/mapApp/static/js/main.39327f25.chunk.js"
+  },
+  {
+    "revision": "69f4cffe198525392c0e",
+    "url": "/KarimA-GIS/mapApp/static/js/runtime-main.8de58db0.js"
+  }
+]);
